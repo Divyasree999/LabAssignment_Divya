b63c6397a3fe43d7eb4b6758a0aca7b4984993e4
Divyasree999 <73293637+Divyasree999@users.noreply.github.com>
2020-11-04 09:39:21 +0530

    Add files via upload

---
+++ LabAssignment5/com/cg/eis/exception/AgeException.java
@@ -0,0 +1,28 @@
+package com.cg.eis.exception;
+/**
+ * User defined exception.
+ * @author Divya Sree
+ *
+ */
+public class AgeException extends Exception{
+	/**
+	 * Initialize the age exception without any message.
+	 */
+	public AgeException() {
+		/**
+		 * Calling the parent class default constructor.
+		 */
+		super();
+	}
+	
+	/**
+	 * Initialize the age exception with message.
+	 * @param msg with which exception has to be initialized.
+	 */
+	public AgeException(String msg) {
+		/**
+		 * Calling the parent class parametrized constructor with provided msg.
+		 */
+		super(msg);
+	}
+}
---
+++ LabAssignment5/com/cg/eis/exception/EmployeeException.java
@@ -0,0 +1,22 @@
+package com.cg.eis.exception;
+/**
+ * A user defined exception class.
+ * @author Divya Sree
+ *
+ */
+public class EmployeeException extends Exception{
+	/**
+	 * For initializing the user defined exception without any message.
+	 */
+	public EmployeeException() {
+		super();
+	}
+	
+	/**
+	 * For initializing the user defined exception with given message.
+	 * @param message for initializing the user defined exception.
+	 */
+	public EmployeeException(String message) {
+		super(message);
+	}
+}
