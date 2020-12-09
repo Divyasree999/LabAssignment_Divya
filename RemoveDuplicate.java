9397a34142a699bc58aae13027e9e8da99f36c77
Divyasree999 <73293637+Divyasree999@users.noreply.github.com>
2020-12-09 21:34:50 +0530

    Lab3assignment

---
+++ RemoveDuplicate.java
@@ -0,0 +1,53 @@
+package lab2;
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+* <h1>Removal of Duplicate from integers</h1>
+* The RemoveDuplicate program implements an application that
+* simply removes all the duplicate elements in the given set of integers and Prints
+* the output on the screen.
+*
+* @author  Divya
+* @date   23-10-2020
+*/
+public class RemoveDuplicate {
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of an array");
+		int n=sc.nextInt();
+		System.out.println("Enter the element of an array");
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		sc.close();
+		n=modifyArray(arr);
+		System.out.println("After Duplicate Element");
+		for (int i=0; i<n; i++) 
+			System.out.print(arr[i]+" "); 
+	}
+
+	private static int modifyArray(int[] arr) {
+		
+		int n=arr.length; //size of an array
+		if (n==0 || n==1){  //array is empty or contain 1 element
+            return n;  
+        }  
+        int[] temp = new int[n]; 
+        Arrays.sort(arr); //sorting array
+        int j = 0;  
+        for (int i=0; i<n-1; i++){  
+            if (arr[i] != arr[i+1]){  
+                temp[j++] = arr[i];  
+            }  
+         }  
+        temp[j++] = arr[n-1];     
+        // Changing original array  
+        for (int i=0; i<j; i++){  
+            arr[i] = temp[i];  
+        }  
+        return j; 
+	 }
+}
---
+++ ReverseSort.java
@@ -0,0 +1,27 @@
+package lab2;
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+* <h1>Reversing and sorting of numbers from given set</h1>
+* The ReverseSort program implements an application that
+* simply reverse the numbers and gives the numbers in sorted manner in the given set of numbers and Prints
+* the output on the screen.
+*
+* @author  Divya
+* @date   23-10-2020
+*/
+public class ReverseSort
+{
+    public static void main(String[] args) 
+	{
+        Scanner s=new Scanner(System.in);
+		int x[]= {10,20,45,6,78}; //Declaration and Initialization of array
+		ReverseSort obj=new ReverseSort();
+		obj.revArray(x);
+    }
+	private void revArray(int[] x) {
+        String str=Arrays.toString(x);
+		Arrays.sort(x);
+	    System.out.println("sorted array is:"+Arrays.toString(x));
+	}
+}
